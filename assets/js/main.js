b54b346f3fedc14545c49ad57b55b215ec78bd62
Juliana Kwan <julianakwan123@gmail.com>
2024-07-02 14:25:17 +0100

    add some more presentations, turn off poptrox

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,18 +81,18 @@
 
 			var $gallery = $('.gallery');
 
-			$gallery.poptrox({
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#1f2328',
-				overlayOpacity: 0.65,
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 50,
-				usePopupNav: true
-			});
+//			$gallery.poptrox({
+//				baseZIndex: 10001,
+//				useBodyOverflow: false,
+//				usePopupEasyClose: false,
+//				overlayColor: '#1f2328',
+//				overlayOpacity: 0.65,
+//				usePopupDefaultStyling: false,
+//				usePopupCaption: true,
+//				popupLoaderText: '',
+//				windowMargin: 50,
+//				usePopupNav: true
+//			});
 
 			// Hack: Adjust margins when 'small' activates.
 				breakpoints.on('>small', function() {
